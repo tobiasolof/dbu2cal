7894b664a5046e16d52c23fec5b388f34ab89998
Tobias Lindberg <lindberg.tob@gmail.com>
2019-03-05 18:22:28 +0000

    Add support for DAI

--- dbu2cal.py
+++ dbu2cal.py
@@ -1,5 +1,6 @@
 import os
 import time
+import re
 from datetime import datetime, timedelta
 from urllib.request import urlopen
 
@@ -9,9 +10,27 @@ from bs4 import BeautifulSoup
 
 
 def build_calendar(url):
+
+    # Set domain-specific paths
+    if 'dbukoebenhavn' in url:
+        domain = 'dbu'
+        class_path = 'dbustandard stripes full srDefault srProgram1 srDefaultPadding'
+        team_path = 'indhold_0_indholdbredvenstre_0_integrationwrapper_1_ctl01_PageTop_TopHeadline2'
+        league_path = 'indhold_0_indholdbredvenstre_0_integrationwrapper_1_ctl01_PageTop_TopHeadline1'
+        result_path = 'Res'
+        date_path = 'Tidspunkt'
+        date_format = '%d-%m-%yKl. %H:%M'
+    elif 'dai-sport' in url:
+        domain = 'dai'
+        class_path = 'srDefault srProgramNormal'
+        team_path = 'ctl00_ContentPlaceHolder1_Hold1_PageTop_MainHeadline'
+        league_path = 'ctl00_ContentPlaceHolder1_Hold1_PageTop_TopHeadline'
+        result_path = 'Resultat'
+        date_path = 'Dato'
+        date_format = '%d-%m-%y  kl. %H:%M'
+
     # Read table into dataframe
-    df = pd.read_html(url, attrs={
-        'class': 'dbustandard stripes full srDefault srProgram1 srDefaultPadding'})[0]
+    df = pd.read_html(url, attrs={'class': class_path}, flavor='bs4')[0]
 
     # Remove first row if it is a duplicate of the header
     if df.iloc[0, 0] == 'Kamp':
@@ -19,9 +38,11 @@ def build_calendar(url):
         df.reset_index(drop=True, inplace=True)
 
     # Correct column names
-    new_header = df.columns.to_series().shift(1)
-    new_header.iat[0] = 'Kampnr'
-    df = df.rename(new_header, axis='columns')
+    if domain == 'dbu':
+        new_header = df.columns.to_series().shift(1)
+        new_header.iat[0] = 'Kampnr'
+        df = df.rename(new_header, axis='columns')
+    df.columns = [col.strip() for col in df.columns]
 
     # Read page into BeautifulSoup (used to get link to match page)
     page = urlopen(url)
@@ -31,14 +52,10 @@ def build_calendar(url):
     cal = icalendar.Calendar()
 
     # Give calendar a relevant name
-    team = soup.find('h1', attrs={
-        'id':
-            'indhold_0_indholdbredvenstre_0_integrationwrapper_1_ctl01_PageTop_TopHeadline2'}).text
+    team = soup.find(attrs={'id': team_path}).text
     # "Kampprogram" is the header if not filtering by team
     team = team + ' ' if team != 'Kampprogram' else ''
-    league = soup.find('h4', attrs={
-        'id':
-            'indhold_0_indholdbredvenstre_0_integrationwrapper_1_ctl01_PageTop_TopHeadline1'}).text
+    league = soup.find(attrs={'id': league_path}).text
     league = league.replace(',', '')
     cal.add('X-WR-CALNAME', team + league)
 
@@ -52,26 +69,32 @@ def build_calendar(url):
 
         # Add title (including result if available)
         event_name = row[1]['Hjemmehold'] + ' - ' + row[1]['Udehold']
-        result = ' (' + row[1]['Res'] + ')' if row[1]['Res'] == row[1]['Res'] else ''
+        result = ' (' + row[1][result_path] + ')' if row[1][result_path] == row[1][result_path] else ''
         temp_event.add('summary', event_name + result)
 
         # Add link to match page
-        link = soup.find('a', attrs={
-            'title': 'Kampinformation for {}'.format(row[1]['Kampnr'])})['href']
-        url_first_part = '/'.join(url.split('/')[:-1])
-        match_url = '/'.join((url_first_part, link))
+        if domain == 'dbu':
+            link = soup.find('a', attrs={
+                'title': 'Kampinformation for {}'.format(row[1]['Kampnr'])})['href']
+            url_first_part = '/'.join(url.split('/')[:-1])
+            match_url = '/'.join((url_first_part, link))
+        elif domain == 'dai':
+            link = soup.find('a', string=str(row[1]['Kampnr.']))['href']
+            url_first_part = '/'.join(url.split('/')[:-3])
+            match_url = ''.join((url_first_part, link))
         temp_event.add('description', match_url)
 
         # Get address from match page
         match_page = urlopen(match_url)
         match_soup = BeautifulSoup(match_page, 'html.parser')
         address = ', '.join(match_soup.find(
-            'td', string='Spillested').find_next_sibling().get_text('///').split('///')[1:3])
+            'td', string=re.compile('\s*Spillested\s*')
+        ).find_next_sibling().get_text('///').split('///')[1:3])
         temp_event.add('location', address)
 
         # Add start and end times
         start_time = datetime.strptime(
-            row[1]['Tidspunkt'], '%d-%m-%yKl. %H:%M')
+            row[1][date_path], date_format)
         end_time = start_time + timedelta(hours=2)
         temp_event.add('dtstart', start_time)
         temp_event.add('dtend', end_time)
